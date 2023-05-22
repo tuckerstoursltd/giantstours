ddc3fac84077ed877f4e8e17c22e4dfd60ef1f49
Denartha10 <mahonniall2012@gmail.com>
2023-05-22 13:48:36 +0100

    added images galore and a new gallery

---
+++ tina/config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "gallery",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/post",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      // {
+      //   name: "gallery",
+      //   label: "Photo Gallery",
+      //   path: "public/gallery",
+      //   fields: [
+      //     {
+      //       type: "image",
+      //     }
+      //   ]
+      // }
+    ],
+  },
+});
